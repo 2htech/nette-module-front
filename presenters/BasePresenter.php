ff7b87ef96b5d6a1f491849f112d5477eeb12955
Tomas Haubert <tomas.haubert@tlapnet.cz>
2019-04-18 16:02:32 +0200

    initial

---
+++ presenters/BasePresenter.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FrontModule\Presenters;
+
+use Nette;
+
+
+/**
+ * Base presenter for all application presenters.
+ */
+abstract class BasePresenter extends \App\CoreModule\Presenters\BasePresenter
+{
+}
